4485e17fee97ea0c33449c874c575473f7a78e89
Larry Huston <larry.huston@intel.com>
2004-03-11 20:52:03 +0000

    More header file munging

--- src/snapfind/snapfind.h
+++ src/snapfind/snapfind.h
@@ -12,7 +12,7 @@ typedef struct thumbnail_t {
 	GtkWidget 	*viewport; /* the viewport that contains the image */
 	GtkWidget 	*gimage; /* the image widget */
 	TAILQ_ENTRY(thumbnail_t) link;
-	char 		name[MAX_NAME];	/* name of image */
+	char 		name[COMMON_MAX_NAME];	/* name of image */
 	int		nboxes;	/* number of histo regions */
 	int             nfaces;	/* number of faces */
 	image_hooks_t   *hooks;
