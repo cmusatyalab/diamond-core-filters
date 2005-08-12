16f5bf25998e1092036c5475f25c8acd8eb3b0b4
Larry Huston <larry@thehustons.net>
2005-08-12 17:53:12 +0000

    Initial checkin

---
+++ src/common/snapfind_consts.h
@@ -0,0 +1,40 @@
+/*
+ * 	SnapFind (Release 0.9)
+ *      An interactive image search application
+ *
+ *      Copyright (c) 2002-2005, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+#ifndef _SNAPFIND_CONSTS_H_
+#define _SNAPFIND_CONSTS_H_	1
+
+
+/* used to get buffers for computing path names */
+#define	COMMON_MAX_PATH		256
+#define SF_MAX_PATH 		COMMON_MAX_NAME
+
+#define	COMMON_MAX_NAME		128
+#define SF_MAX_NAME 		COMMON_MAX_NAME
+
+                                                                                
+/* max number of regions that can be selected in zoomed window */
+#define MAX_SELECT 32
+
+/* number of albums we support */
+#define MAX_ALBUMS 32
+
+/* presumed max number of active filters */
+#define SF_MAX_FILTERS 	64
+
+/* presumed max number of active devices */
+#define SF_MAX_DEVICES 	64
+
+
+#endif	/* ! _SNAPFIND_CONSTS_H_ */
+
