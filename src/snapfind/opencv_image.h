82625f2becbb919e213de737c83cccaacfb84ac9
Larry Huston <larry@thehustons.net>
2005-08-19 17:28:31 +0000

    More code cleanup

---
+++ src/snapfind/opencv_image.h
@@ -0,0 +1,42 @@
+/*
+ * 	SnapFind (Release 0.9)
+ *      An interactive image search application
+ *
+ *      Copyright (c) 2002-2005, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+#ifndef	_OPENCV_IMAGE_H_
+#define	_OPENCV_IMAGE_H_	1
+
+#include <stdint.h>
+#include <errno.h>
+#include "rgb.h"
+#include <opencv/cv.h>
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+
+IplImage *create_gray_ipl_image(char *filename);
+IplImage *create_rgb_ipl_image(char *filename);
+
+IplImage *get_gray_ipl_image(RGBImage* rgb_img);
+IplImage *get_rgb_ipl_image(RGBImage* rgb_img);
+
+
+RGBImage * convert_ipl_to_rgb(IplImage * ipl);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+#endif                          /* ! _OPENCV_IMAGE_H_ */
