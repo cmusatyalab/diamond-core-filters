ab536e4ac56e7dc183a30a12be2ff27bc7ed9345
Larry Huston <larry@thehustons.net>
2006-02-16 20:58:37 +0000

    Fix bug with some linux variants that return file type DT_UNKNOWN from readdir.

--- src/snapfind/plugin.cc
+++ src/snapfind/plugin.cc
@@ -11,6 +11,16 @@
  *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
  */
 
+/*
+ *  Copyright (c) 2006 Larry Huston <larry@thehustons.net>
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+
 #include <pthread.h>
 #include <sys/time.h>
 #include <sys/types.h>
@@ -151,8 +161,12 @@ load_plugins()
 
         while ((cur_ent = readdir(dir)) != NULL) {
 
-		/* If this isn't a file then we skip the entry.  */
-                if ((cur_ent->d_type != DT_REG)) {
+		/* we skip non-files.  It appears that some versions of
+		 * linux report the file type as UNKNOWN.  In that
+		 * case we don't skip.
+		 */
+                if ((cur_ent->d_type != DT_REG) && 
+		    (cur_ent->d_type != DT_UNKNOWN)) {
                         continue;
                 }
                                                                                 
