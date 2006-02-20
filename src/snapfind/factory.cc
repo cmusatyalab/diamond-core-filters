1512e3df891dcd263c2c992ccc4bf6923b4464c3
Larry Huston <larry@thehustons.net>
2006-02-20 05:08:17 +0000

    Fix bug in factory enumeration when list is NULL.

--- src/snapfind/factory.cc
+++ src/snapfind/factory.cc
@@ -1,3 +1,26 @@
+/*
+ * 	SnapFind (Release 0.9)
+ *      An interactive image search application
+ *
+ *      Copyright (c) 2002-2005, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+/*
+ *  Copyright (c) 2006 Larry Huston <larry@thehustons.net>
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+
 #include <string.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -58,8 +81,8 @@ find_support_factory(const char *name)
 {
 	factory_map_t *cur_map;
 
-	for (cur_map = support_fmap; cur_map != NULL; cur_map = cur_map->fm_next) {
-
+	for (cur_map = support_fmap; cur_map != NULL;
+	    cur_map = cur_map->fm_next) {
 		if (strcmp(name, cur_map->fm_factory->get_description()) == 0) {
 			return(cur_map->fm_factory);
 		}
@@ -87,8 +110,13 @@ get_first_factory(void **cookie)
 	factory_map_t *cur_map;
 
 	cur_map = fmap;
-	*cookie = (void *)cur_map;
-	return(cur_map->fm_factory);
+	if (cur_map == NULL) {
+		*cookie = NULL;
+		return(NULL);
+	} else {
+		*cookie = (void *)cur_map;
+		return(cur_map->fm_factory);
+	}
 }
 
 img_factory *
