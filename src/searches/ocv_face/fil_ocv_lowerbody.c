90600738cc89f3e4419842c8364a15461715f287
Lily Mummert <lily.b.mummert@intel.com>
2006-04-06 20:36:39 +0000

    lower body detection using opencv 0.97

---
+++ src/searches/ocv_face/fil_ocv_lowerbody.c
@@ -0,0 +1,65 @@
+/*
+ * 	SnapFind (Release 0.9)
+ *      An interactive image search application
+ *
+ *      Copyright (c) 2002-2005, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+/*
+ *  Copyright (c) 2006 Larry Huston <larry@thehustons.net>
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+
+/*
+ * lower body detector 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <math.h>
+#include <assert.h>
+#include <unistd.h>
+#include <opencv/cvaux.h>
+
+#include "lib_filter.h"
+#include "fil_ocv_common.h"
+#include "fil_ocv_lowerbody.h"
+
+
+int
+f_init_opencv_lbdetect(int numarg, char **args, int blob_len, void *blob_data,
+ 			const char *fname, void **fdatap)
+{
+        return f_init_opencv_detect(numarg, args, blob_len, blob_data, fname, fdatap);
+}
+
+
+int
+f_fini_opencv_lbdetect(void *fdata)
+{
+	return f_fini_opencv_detect(fdata);
+}
+
+
+int
+f_eval_opencv_lbdetect(lf_obj_handle_t ohandle, void *fdata)
+{
+        return f_eval_opencv_detect(ohandle, fdata);
+}
+
+
+
+
---
+++ src/searches/ocv_face/ocv_lowerbody_search.cc
@@ -0,0 +1,121 @@
+/*
+ * 	SnapFind (Release 0.9)
+ *      An interactive image search application
+ *
+ *      Copyright (c) 2002-2005, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <gtk/gtk.h>
+#include <opencv/cv.h>
+#include <opencv/cvaux.h>
+#include <sys/stat.h>
+
+#include "queue.h"
+#include "rgb.h"
+#include "lib_results.h"
+#include "snapfind_consts.h"
+#include "img_search.h"
+#include "ocv_lowerbody_search.h"
+#include "opencv_face_tools.h"
+#include "search_set.h"
+#include "factory.h"
+#include "snapfind_config.h"
+
+#define	SEARCH_NAME	"ocv_lowerbody_search"
+#define CLASSIFIER_NAME       "haarcascade_lowerbody"
+
+/* config tokens  */
+#define	NUMFACE_ID	"NUMFACE"
+#define	SUPPORT_ID	"SUPPORT"
+
+
+extern "C" {
+	void search_init();
+}
+
+void
+search_init()
+{
+	ocv_lowerbody_factory *fac;
+
+	fac = new ocv_lowerbody_factory;
+	factory_register(fac);
+}
+
+
+ocv_lowerbody_search::ocv_lowerbody_search(const char *name, char *descr)
+		: ocv_search(name, descr)
+{
+	set_scale(1.20);
+	set_stride(1);
+	set_testx(24);
+	set_testy(24);
+	set_classifier(CLASSIFIER_NAME);
+}
+
+ocv_lowerbody_search::~ocv_lowerbody_search()
+{
+        printf("destroying OCV lowerbody");
+	return;
+}
+
+void
+ocv_lowerbody_search::write_config(FILE *ostream, const char *dirname)
+{
+	save_edits();
+
+	/* create the search configuration */
+	fprintf(ostream, "\n\n");
+	fprintf(ostream, "SEARCH %s %s\n", SEARCH_NAME, get_name());
+	fprintf(ostream, "%s %d \n", NUMFACE_ID, get_count());
+	fprintf(ostream, "%s %d \n", SUPPORT_ID, get_support());
+	window_search::write_config(ostream, dirname);
+	return;
+}
+
+void ocv_lowerbody_search::write_fspec(FILE *ostream) 
+{
+	/*
+	 * First we write the header section that corresponds
+	 * to the filter, the filter name, the associated functions.
+	 */
+	save_edits();
+	fprintf(ostream, "\n");
+	fprintf(ostream, "FILTER %s \n", get_name());
+
+	fprintf(ostream, "EVAL_FUNCTION  %s \n", "f_eval_opencv_lbdetect");
+	fprintf(ostream, "INIT_FUNCTION  %s \n", "f_init_opencv_lbdetect");
+	fprintf(ostream, "FINI_FUNCTION  %s \n", "f_fini_opencv_lbdetect");
+
+	ocv_search::write_fspec(ostream);
+	return;
+}
+
+int 
+ocv_lowerbody_search::matches_filter(char *name) 
+{
+	void *handle;
+	void *initfn = NULL;
+                                                                                
+	handle = dlopen(name, RTLD_NOW);
+	if (handle) {
+		initfn = dlsym(handle, "f_init_opencv_lbdetect");
+		dlclose(handle);
+	}
+	
+	return ((int) initfn);
+}
+
