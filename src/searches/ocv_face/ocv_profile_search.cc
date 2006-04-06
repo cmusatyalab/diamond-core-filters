c43c87f50b1e992a915b51406c1abad6d5bb4f3f
Lily Mummert <lily.b.mummert@intel.com>
2006-04-06 20:37:29 +0000

    face profile detection using opencv 0.97

---
+++ src/searches/ocv_face/ocv_profile_search.cc
@@ -0,0 +1,121 @@
+/*
+ * 	SnapFind (Release 0.9)
+ *      An interactive image search application
+ *
+ *      Copyright (c) 2002-2005, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dlfcn.h>
+#include <gtk/gtk.h>
+#include <opencv/cv.h>
+#include <opencv/cvaux.h>
+#include <sys/stat.h>
+
+#include "queue.h"
+#include "rgb.h"
+#include "lib_results.h"
+#include "snapfind_consts.h"
+#include "img_search.h"
+#include "ocv_profile_search.h"
+#include "opencv_face_tools.h"
+#include "search_set.h"
+#include "factory.h"
+#include "snapfind_config.h"
+
+#define	SEARCH_NAME	"ocv_profile_search"
+#define CLASSIFIER_NAME       "haarcascade_profileface"
+
+/* config tokens  */
+#define	NUMFACE_ID	"NUMFACE"
+#define	SUPPORT_ID	"SUPPORT"
+
+
+extern "C" {
+	void search_init();
+}
+
+void
+search_init()
+{
+	ocv_profile_factory *fac;
+
+	fac = new ocv_profile_factory;
+	factory_register(fac);
+}
+
+
+ocv_profile_search::ocv_profile_search(const char *name, char *descr)
+		: ocv_search(name, descr)
+{
+	set_scale(1.20);
+	set_stride(1);
+	set_testx(24);
+	set_testy(24);
+	set_classifier(CLASSIFIER_NAME);
+}
+
+ocv_profile_search::~ocv_profile_search()
+{
+        printf("destroying OCV profile");
+	return;
+}
+
+void
+ocv_profile_search::write_config(FILE *ostream, const char *dirname)
+{
+	save_edits();
+
+	/* create the search configuration */
+	fprintf(ostream, "\n\n");
+	fprintf(ostream, "SEARCH %s %s\n", SEARCH_NAME, get_name());
+	fprintf(ostream, "%s %d \n", NUMFACE_ID, get_count());
+	fprintf(ostream, "%s %d \n", SUPPORT_ID, get_support());
+	window_search::write_config(ostream, dirname);
+	return;
+}
+
+void ocv_profile_search::write_fspec(FILE *ostream) 
+{
+	/*
+	 * First we write the header section that corresponds
+	 * to the filter, the filter name, the associated functions.
+	 */
+	save_edits();
+	fprintf(ostream, "\n");
+	fprintf(ostream, "FILTER %s \n", get_name());
+
+	fprintf(ostream, "EVAL_FUNCTION  %s \n", "f_eval_opencv_pdetect");
+	fprintf(ostream, "INIT_FUNCTION  %s \n", "f_init_opencv_pdetect");
+	fprintf(ostream, "FINI_FUNCTION  %s \n", "f_fini_opencv_pdetect");
+
+	ocv_search::write_fspec(ostream);
+	return;
+}
+
+int 
+ocv_profile_search::matches_filter(char *name) 
+{
+	void *handle;
+	void *initfn = NULL;
+                                                                                
+	handle = dlopen(name, RTLD_NOW);
+	if (handle) {
+		initfn = dlsym(handle, "f_init_opencv_pdetect");
+		dlclose(handle);
+	}
+	
+	return ((int) initfn);
+}
+
---
+++ src/searches/ocv_face/ocv_profile_search.h
@@ -0,0 +1,60 @@
+/*
+ * 	SnapFind (Release 0.9)
+ *      An interactive image search application
+ *
+ *      Copyright (c) 2002-2005, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+#ifndef	_OCV_PROFILE_SEARCH_H_
+#define	_OCV_PROFILE_SEARCH_H_	1
+
+#include "snapfind_consts.h"
+#include "ocv_search.h"
+
+
+class ocv_profile_search: public ocv_search {
+public:
+	ocv_profile_search(const char *name, char *descr);
+	virtual ~ocv_profile_search();
+
+	void	write_config(FILE* stream, const char *data_dir);
+	void    write_fspec(FILE* stream);
+	int     matches_filter(char *name);
+};
+
+class ocv_profile_factory: public ocv_factory {
+public:
+	ocv_profile_factory() {
+		set_name("Face (profile)");
+		set_description("ocv_profile_search");
+	}
+
+	img_search *create(const char *name) {
+		return new ocv_profile_search(name, "Face (profile)");
+	}
+	int is_example() {
+		return(0);
+	}
+};
+
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* this needs to be provide by someone calling this library */
+void ss_add_dep(img_search *dep);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* !_OCV_PROFILE_SEARCH_H_ */
