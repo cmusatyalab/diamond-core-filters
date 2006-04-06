e750b87b7529841fc974b7981f693203a18a0967
Lily Mummert <lily.b.mummert@intel.com>
2006-04-06 20:33:49 +0000

    body detection using opencv 0.97

---
+++ src/searches/ocv_face/fil_ocv_body.h
@@ -0,0 +1,33 @@
+/*
+ * 	SnapFind (Release 0.9)
+ *      An interactive image search application
+ *
+ *      Copyright (c) 2002-2005, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+
+#ifndef	_FIL_OCV_BODY_H_
+#define	_FIL_OCV_BODY_H_ 1
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+int f_eval_opencv_bdetect(lf_obj_handle_t ohandle, void *fdata);
+
+int f_fini_opencv_bdetect(void *fdata);
+
+int f_init_opencv_bdetect(int numarg, char **args, int blob_len, 
+	void *blob_data, const char *fname, void **fdatap);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* !_FIL_OCV_BODY_H_ */
---
+++ src/searches/ocv_face/ocv_body_search.h
@@ -0,0 +1,60 @@
+/*
+ * 	SnapFind (Release 0.9)
+ *      An interactive image search application
+ *
+ *      Copyright (c) 2002-2005, Intel Corporation
+ *      All Rights Reserved
+ *
+ *  This software is distributed under the terms of the Eclipse Public
+ *  License, Version 1.0 which can be found in the file named LICENSE.
+ *  ANY USE, REPRODUCTION OR DISTRIBUTION OF THIS SOFTWARE CONSTITUTES
+ *  RECIPIENT'S ACCEPTANCE OF THIS AGREEMENT
+ */
+
+#ifndef	_OCV_BODY_SEARCH_H_
+#define	_OCV_BODY_SEARCH_H_	1
+
+#include "snapfind_consts.h"
+#include "ocv_search.h"
+
+
+class ocv_body_search: public ocv_search {
+public:
+	ocv_body_search(const char *name, char *descr);
+	~ocv_body_search(void);
+
+	void	write_config(FILE* stream, const char *data_dir);
+	void    write_fspec(FILE* stream);
+	int     matches_filter(char *name);
+};
+
+class ocv_body_factory: public ocv_factory {
+public:
+	ocv_body_factory() {
+		set_name("Body");
+		set_description("ocv_body_search");
+	}
+
+	img_search *create(const char *name) {
+		return new ocv_body_search(name, "Body");
+	}
+	int is_example() {
+		return(0);
+	}
+};
+
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* this needs to be provide by someone calling this library */
+void ss_add_dep(img_search *dep);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif	/* !_OCV_BODY_SEARCH_H_ */
