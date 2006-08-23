655c465547c58fa6bcf9c3ce52e2c11ca48688f6
Lily Mummert <lily.b.mummert@intel.com>
2006-08-23 16:16:47 +0000

    add interface to read rgb image from memory buffer

--- src/lib/libsfimage/lib_sfimage.h
+++ src/lib/libsfimage/lib_sfimage.h
@@ -32,12 +32,14 @@ extern "C"
 #endif
 
 /*
- * some interfaces to read images from a path/file
+ * some interfaces to read images from a path/file/buffer
  */
 
 RGBImage       *get_rgb_img(lf_obj_handle_t ohandle);
+RGBImage	   *read_rgb_image(unsigned char *buf, size_t buflen);
 
-RGBImage *create_rgb_image(const char *filename);
+RGBImage 	*create_rgb_image(const char *filename);
+
 int rgb_write_image(RGBImage *img, const char *filename, const char *path);
 
 
