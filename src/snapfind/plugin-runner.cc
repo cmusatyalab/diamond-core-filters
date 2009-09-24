d005c1b47bcbef0c410d9235ebc8cd205c3ebe35
Adam Goode <agoode@andrew.cmu.edu>
2009-09-24 16:58:35 -0400

    Add more to edit_plugin_config, and also print out is-editable from initial-config

--- src/snapfind/plugin-runner.cc
+++ src/snapfind/plugin-runner.cc
@@ -31,6 +31,16 @@ print_key_value(const char *key,
 }
 
 static void
+print_key_value(const char *key,
+		int value_len,
+		void *value) {
+	printf("K %d\n%s\n", strlen(key), key);
+	printf("V %d\n", value_len);
+	fwrite(value, value_len, 1, stdout);
+	printf("\n");
+}
+
+static void
 print_plugin(const char *type, img_factory *imgf) {
 	print_key_value("type", type);
 	print_key_value("display-name", imgf->get_name());
@@ -59,31 +69,54 @@ list_plugins(void) {
 	}
 }
 
-
-int
-get_plugin_initial_config(const char *type,
-			  const char *internal_name) {
+static img_search *get_plugin(const char *type,
+			      const char *internal_name) {
 	img_factory *imgf;
 
-	// find plugin
 	if (sc(type, "filter")) {
 		imgf = find_factory(internal_name);
 	} else if (sc(type, "codec")) {
 		imgf = find_codec_factory(internal_name);
 	} else {
 		printf("Invalid type\n");
-		return 1;
+		return NULL;
 	}
 
-	if (imgf == NULL) {
+	if (!imgf) {
+	  return NULL;
+	}
+
+	return imgf->create("filter");
+}
+
+static void
+print_search_config(img_search *search) {
+	// editable?
+	print_key_value("is-editable", search->is_editable() ? "true" : "false");
+
+	// print blob
+	print_key_value("blob", search->get_auxiliary_data_length(),
+			search->get_auxiliary_data());
+
+	// newline separator
+	printf("\n");
+
+	// print config
+	if (search->is_editable()) {
+		search->write_config(stdout, NULL);
+	}
+}
+
+int
+get_plugin_initial_config(const char *type,
+			  const char *internal_name) {
+	img_search *search = get_plugin(type, internal_name);
+	if (search == NULL) {
 		printf("Can't find %s\n", internal_name);
 		return 1;
 	}
 
-	img_search *search = imgf->create("_");
-	if (search->is_editable()) {
-		search->write_config(stdout, NULL);
-	}
+	print_search_config(search);
 
 	return 0;
 }
@@ -91,5 +124,24 @@ get_plugin_initial_config(const char *type,
 int
 edit_plugin_config(const char *type,
 		   const char *internal_name) {
-  return 1;
+	img_search *search = get_plugin(type, internal_name);
+	if (search == NULL) {
+		printf("Can't find %s\n", internal_name);
+		return 1;
+	}
+
+	if (!search->is_editable()) {
+		printf("Not editable");
+		return 1;
+	}
+
+	// TODO take in config from user
+
+	search->set_exit_gui_on_close_edit_win(true);
+	search->edit_search();
+	gtk_main();
+
+	print_search_config(search);
+
+	return 0;
 }
