101744b7c0f010272b7eca80785a7e2dbeddebb8
Benjamin Gilbert <bgilbert@cs.cmu.edu>
2011-06-17 18:33:03 -0400

    Remove unused definitions from lib_results.h

--- include/lib_results.h
+++ include/lib_results.h
@@ -17,7 +17,6 @@
 #define _LIB_RESULTS_H_	1
 
 #include <sys/types.h>
-#include <errno.h>
 #include <stdint.h>
 #include <sys/queue.h>
 
@@ -25,21 +24,6 @@
 
 /* image dimension */
 typedef int32_t dim_t;
-
-/*
- * bounding box of region to consider
- */
-typedef struct region {
-	dim_t xmin, ymin;
-	dim_t xsiz, ysiz;
-} region_t;
-
-typedef enum param_type_t {
-    PARAM_UNKNOWN = 0,
-    PARAM_FACE,
-    PARAM_HISTO,
-    PARAM_TEXTURE
-} param_type_t;
 
 typedef struct patch {
     int                                 min_x;
@@ -61,25 +45,6 @@ typedef TAILQ_HEAD(bbox_list_t, bbox)   bbox_list_t;
 
 
 
-/*
- * attributes passed to the test function
- */
-#define PARAM_NAME_MAX 15
-typedef struct search_param
-{
-        param_type_t type;
-        int lev1, lev2;         /* range of tests to apply (face) */
-        region_t bbox;          /* bounding box */
-        double scale;           /* (face) */
-        union {
-                double img_var; /* (face) */
-                double distance; /* (histo) */
-        };
-        char name[PARAM_NAME_MAX+1];
-        int id;
-} search_param_t;
-
-
 typedef struct img_patches {
 	int		num_patches;
 	double		distance;
@@ -94,41 +59,16 @@ typedef struct img_patches {
  * some more attributes
  */
 
-//#define NUM_BBOXES "_num_bboxes.int"
-//#define BBOX_FMT   "_bbox%d.search_param"
-
 #define NUM_HISTO        "_nhisto_passed.int"
-#define NUM_FACE         "_nface_passed.int"
-#define NUM_TEXTURE      "_ntexture_passed.int"
-#define HISTO_BBOX_FMT   "_h_box%d.search_param"
-#define FACE_BBOX_FMT    "_f_box%d.search_param"
 
 #define FILTER_MATCHES  "_filter.%s.patches"
 
-#define POISON_FN  "poison_%s"
-
-#define II_DATA    "_integral_img.ii_image"
-#define II_SQ_DATA "_integral_img_sq.ii_image"
 #define RGB_IMAGE  "_rgb_image.rgbimage"
 #define HISTO_II   "integral_histo"
 
 #define ROWS       "_rows.int"
 #define COLS       "_cols.int"
-#define IMG_HEADERLEN "_headerlen.int"
 
-#define RGBA_IPL_IMAGE "_rgba_image.ipl_image"
-#define GRAY_IPL_IMAGE "_gray_image.ipl_image"
-
-
-#define KEYWORDS "Keywords"
-#define CONTENT_TYPE "Content-Type"
-#define PARENT_OIT "Parent-OID"
-
-
-
-#define FILTER_TYPE_COLOR 0
-#define FILTER_TYPE_TEXTURE 1
-#define FILTER_TYPE_ARENA 2
 
 #ifdef	__cplusplus
 extern "C" {
